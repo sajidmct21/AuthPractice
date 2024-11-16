20f94041e8a17f3223cb39e356da34116c5831ee
sajid yaqub <sajidmct21@gmail.com>
2024-11-16 14:04:37 +0500

    Update Users Routes file with asyncHandler function

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,7 +7,7 @@ import {
   updateUser,
 } from "../controllers/user.controller.js";
 import { asyncHandlerPromise } from "../utils/asyncHandler.js";
-import { verifyAdmin } from "../middlewares/auth.middleware.js";
+import { verifyAdmin, verifyUser } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
@@ -15,15 +15,15 @@ const router = express.Router();
 router.route("/createUser").post(createUser);
 
 // get all users
-router.route("/getAllUsers").get(asyncHandlerPromise(verifyAdmin), getAllUsers);
+router.route("/getAllUsers").get(asyncHandlerPromise(verifyAdmin), asyncHandlerPromise(getAllUsers));
 
 // get user by id
-router.route("/getUserById/:id").get(asyncHandlerPromise(getUserById));
+router.route("/getUserById/:id").get(asyncHandlerPromise(verifyUser),asyncHandlerPromise(getUserById));
 
 // update user
-router.route("/updateUser/:id").put(updateUser);
+router.route("/updateUser/:id").put(asyncHandlerPromise(verifyUser),asyncHandlerPromise(updateUser));
 
 // delete a user
-router.route("/deleteUser/:id").delete(deleteUser);
+router.route("/deleteUser/:id").delete(asyncHandlerPromise(verifyAdmin),asyncHandlerPromise(deleteUser));
 
 export default router;

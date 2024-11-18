572f68a11bc4ca8eb4554922dfc927c006242267
sajid yaqub <sajidmct21@gmail.com>
2024-11-18 23:19:09 +0500

    change the MONGO_URI

--- src/dbconnection.js
+++ src/dbconnection.js
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
-// import { DB_NAME } from "./constants.js";
+import { DB_NAME } from "./constants.js";
 
 const dbConnection = async () => {
   try {
     // eslint-disable-next-line no-undef
-    const dbInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
+    const dbInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
     console.log(`DB Instance is running at ${dbInstance.connection.host}`);
   } catch (error) {
     console.log(`DB connection is failed with error ${error}`);
